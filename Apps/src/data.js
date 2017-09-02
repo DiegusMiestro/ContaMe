e10a65391b27f6a22658c02eaa6df7fe68188e63
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-02 12:58:27 -0300

    Update Temporary Data

---
+++ Apps/src/data.js
@@ -0,0 +1,35 @@
+export default {
+  months: [
+    {'id': 1, 'title': 'Janeiro', 'alias': 'JAN'},
+    {'id': 2, 'title': 'Fevereiro', 'alias': 'FEV'},
+    {'id': 3, 'title': 'Março', 'alias': 'MAR'},
+    {'id': 4, 'title': 'Abril', 'alias': 'ABR'},
+    {'id': 5, 'title': 'Maio', 'alias': 'MAI'},
+    {'id': 6, 'title': 'Junho', 'alias': 'JUN'},
+    {'id': 7, 'title': 'Julho', 'alias': 'JUL'},
+    {'id': 8, 'title': 'Agosto', 'alias': 'AGO'},
+    {'id': 9, 'title': 'Setembro', 'alias': 'SET'},
+    {'id': 10, 'title': 'Outubro', 'alias': 'OUT'},
+    {'id': 11, 'title': 'Novembro', 'alias': 'NOV'},
+    {'id': 12, 'title': 'Dezembro', 'alias': 'DEZ'}
+  ],
+  payments: [
+    {'year': 2017, 'month': 8, 'description': 'Cartão de Crédito', 'value': -300.00},
+    {'year': 2017, 'month': 8, 'description': 'Internet', 'value': -150.00},
+    {'year': 2017, 'month': 8, 'description': 'Conta de Água', 'value': -70.00},
+    {'year': 2017, 'month': 8, 'description': 'Conta de Luz', 'value': -120.00},
+    {'year': 2017, 'month': 8, 'description': 'Curso', 'value': -200.00},
+    {'year': 2017, 'month': 8, 'description': 'Corte de Cabelo', 'value': -15.00},
+    {'year': 2017, 'month': 8, 'description': 'Sorvete', 'value': -18.50},
+    {'year': 2017, 'month': 8, 'description': 'Casa Pio', 'value': -50.00},
+    {'year': 2017, 'month': 8, 'description': 'Almoço', 'value': -16.00},
+    {'year': 2017, 'month': 8, 'description': 'Pizza', 'value': -24.00},
+    {'year': 2017, 'month': 8, 'description': 'Almoço', 'value': -16.00},
+    {'year': 2017, 'month': 8, 'description': 'Almoço', 'value': -16.00},
+    {'year': 2017, 'month': 8, 'description': 'Plano Dentário', 'value': -15.00},
+    {'year': 2017, 'month': 8, 'description': 'Ortodontia', 'value': -70.00},
+    {'year': 2017, 'month': 8, 'description': 'Almoço', 'value': -16.00},
+    {'year': 2017, 'month': 8, 'description': 'Almoço', 'value': -16.00},
+    {'year': 2017, 'month': 8, 'description': 'Salário', 'value': 1000.00}
+  ]
+}
