9a69fba86ebf344680e9572d5d4cb2fec35cf600
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-02 12:59:13 -0300

    Add route

--- Apps/src/router/index.js
+++ Apps/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Invoice from '@/components/Invoice'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/invoices',
+      name: 'Invoice',
+      component: Invoice
     }
   ]
 })
