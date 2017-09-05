332e38ac8d25356af139d56f1a69c622828e17ad
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-05 18:00:05 -0300

    Create Component Home

---
+++ Apps/client/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home">
+
+    <h1>Dashboard</h1>
+
+    <p>Página Inicial do ConteMe</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
