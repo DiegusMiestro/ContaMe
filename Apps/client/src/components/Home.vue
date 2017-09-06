6a22d595859b42177680283d285717a3d9f6e726
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-06 18:13:33 -0300

    Only Test

--- Apps/client/src/components/Home.vue
+++ Apps/client/src/components/Home.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="home">
 
-    <h1>Dashboard</h1>
+    <h1>Aprendendo Quasar Framework</h1>
 
-    <p>Página Inicial do ConteMe</p>
+    <q-icon name="icon-chrome" />
+
+    <!-- Material Icon -->
+    <q-icon name="thumb_up"  class="big-icon" />
+
+    <div class="row justify-around">
+      <a href="primeira/" class="col">Primeira Coluna!</a>
+      <a href="segunda/" class="col">Segunda Coluna!!</a>
+      <a href="terceira/" class="col">Terceira Coluna!!!</a>
+      <a href="quarta/" class="col">Quarta Coluna!!!!</a>
+      <a href="quinta/" class="col">Quinta Coluna!!!!!</a>
+    </div>
 
   </div>
 </template>
 
 <script>
+// import data from '../data'
+import { QIcon } from 'quasar'
 export default {
+  components: {
+    QIcon
+  },
   data () {
-    return {}
+    return {
+      // 'months': data.months,
+      // 'payments': data.payments
+    }
   }
 }
 </script>
