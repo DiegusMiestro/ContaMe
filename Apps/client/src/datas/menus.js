8bd02287c2f83fed87d1ca55540b4b5ce8f91a8b
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-06 18:12:05 -0300

    Add: temp data menus

---
+++ Apps/client/src/datas/menus.js
@@ -0,0 +1,7 @@
+export default {
+  navigate: [
+    {'link': '/', 'icon': 'home', 'label': 'Inicio', 'sublabel': 'Página inicial'},
+    {'link': 'payments/', 'icon': 'payment', 'label': 'Pagamentos', 'sublabel': 'Lista de pagamentos'},
+    {'link': 'payments/plots/', 'icon': 'access_time', 'label': 'Parcelas', 'sublabel': 'Parcelas em aberto'}
+  ]
+}
