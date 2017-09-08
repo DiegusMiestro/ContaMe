5f7497e03515aa4c14ff500dfcfea47603ec616b
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-08 18:00:54 -0300

    Temp Data

--- Apps/client/src/datas/menus.js
+++ Apps/client/src/datas/menus.js
@@ -2,6 +2,9 @@ export default {
   navigate: [
     {'link': '/', 'icon': 'home', 'label': 'Inicio', 'sublabel': 'Página inicial'},
     {'link': '/payments/', 'icon': 'payment', 'label': 'Pagamentos', 'sublabel': 'Lista de pagamentos'},
-    {'link': '/payments/plots/', 'icon': 'access_time', 'label': 'Parcelas', 'sublabel': 'Parcelas em aberto'}
+    {'link': '/payments/plots/', 'icon': 'access_time', 'label': 'Parcelas Futuras', 'sublabel': 'Parcelas em aberto'}
+  ],
+  tabs: [
+    {'link': 'add/', 'icon': 'add_circle', 'label': 'Adicionar'}
   ]
 }
