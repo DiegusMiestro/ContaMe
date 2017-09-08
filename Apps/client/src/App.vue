71e7c2ca0c6b6509c9aa57bf08a405ed7e7ac870
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-08 14:10:10 -0300

    Update: Menu Loop

--- Apps/client/src/App.vue
+++ Apps/client/src/App.vue
@@ -2,9 +2,9 @@
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
 
-    <q-layout ref="layout" view="hHh LpR fFf" :right-breakpoint="1100">
+    <q-layout ref="layout" view="lHh LpR lFf" :right-breakpoint="1100">
       <!-- Header -->
-      <q-toolbar slot="header">
+      <q-toolbar slot="header" color="primary">
         <q-btn flat @click="$refs.layout.toggleLeft()">
           <q-icon name="menu" />
         </q-btn>
@@ -12,32 +12,22 @@
           Sistema ContaMe
           <span slot="subtitle">Controle de contas pessoais</span>
         </q-toolbar-title>
-        <q-btn flat @click="$refs.layout.toggleRight()">
-          <q-icon name="menu" />
-        </q-btn>
       </q-toolbar>
 
       <!-- Navigation -->
       <q-tabs slot="navigation">
-        <q-route-tab slot="title" icon="view_quilt" to="/test-layout/about" replace hide="icon" label="About" />
-        <q-route-tab slot="title" icon="view_day" to="/test-layout/toolbar" replace hide="icon" label="Toolbar" />
-        <q-route-tab slot="title" icon="view_day" to="/test-layout/tabs" replace label="Tabs" />
-        <q-route-tab slot="title" icon="input" to="/test-layout/drawer" replace label="Drawer" />
+        <q-route-tab v-for="(nav, index) in navigate" :key="index" slot="title" :icon="nav.icon" :to="nav.link" replace hide="icon" :label="nav.label" />
       </q-tabs>
 
       <!-- Left Side Panel -->
       <div slot="left">
         <q-list no-border link inset-separator>
           <q-list-header>Essential Links</q-list-header>
-          <q-side-link item to="/docs" v-for="nav in navigate">
-            <q-item-side icon="school" />
-            <q-item-main label="Docs" sublabel="quasar-framework.org" />
+          <q-side-link item v-for="(nav, index) in navigate" :key="index" :to="nav.link">
+            <q-item-side :icon="nav.icon" />
+            <q-item-main :label="nav.label" :sublabel="nav.sublabel" />
           </q-side-link>
         </q-list>
-      </div>
-      <!-- Right Side Panel -->
-      <div slot="right">
-        Right Side of Layout
       </div>
 
       <router-view />
