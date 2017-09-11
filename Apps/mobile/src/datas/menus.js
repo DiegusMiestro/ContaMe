db6b6e7337ac986b7697b644b95e538ba1fbf8e5
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-11 18:00:46 -0300

    Create: Temporary Data

---
+++ Apps/mobile/src/datas/menus.js
@@ -0,0 +1,7 @@
+export default {
+  navigate: [
+    {'label': 'Pagamentos', 'sublabel': 'Últimos Pagamentos', 'url': '/payments/', 'icon': 'collections_bookmarks'},
+    {'label': 'Mensal', 'sublabel': 'Pagamentos do mês', 'url': '/Month/', 'icon': 'date_ranger'},
+    {'label': 'Parcelas', 'sublabel': 'Pagamentos Parcelados', 'url': '/Monthly/', 'icon': 'credit_card'}
+  ]
+}
---
+++ Apps/mobile/src/datas/payments.js
@@ -0,0 +1,24 @@
+export default {
+  everything: [
+    {
+      'amount': -40.00,
+      'description': 'Netflix',
+      'date': '2017-09-11'
+    },
+    {
+      'amount': -200.00,
+      'description': 'Pós-graduação',
+      'date': '2017-09-11'
+    },
+    {
+      'amount': -149.99,
+      'description': 'Internet',
+      'date': '2017-09-10'
+    },
+    {
+      'amount': -150.70,
+      'description': 'Jantar de Aniversário de Fulano',
+      'date': '2017-09-08'
+    }
+  ]
+}
