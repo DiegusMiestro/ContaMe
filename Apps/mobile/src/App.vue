1b33aabc754af1a92862f1106c0a10dd53dd0a80
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-11 15:16:31 -0300

    Update: Layout Default

--- Apps/mobile/src/App.vue
+++ Apps/mobile/src/App.vue
@@ -1,15 +1,89 @@
 <template>
-  <!-- Don't drop "q-app" class -->
-  <div id="q-app">
-    <router-view />
-  </div>
+  <q-layout ref="layout" view="lHh Lpr fff" :left-class="{'bg-grey-2': true}" >
+    <q-toolbar slot="header" class="">
+      <q-btn flat @click="$refs.layout.toggleLeft()" >
+        <q-icon name="menu" />
+      </q-btn>
+      <q-toolbar-title>
+        ContaMe App
+        <div slot="subtitle">Sistema de Gestão Financeira Doméstica</div>
+      </q-toolbar-title>
+    </q-toolbar>
+
+    <div slot="left">
+      <q-list no-border link inset-delimiter>
+        <q-list-header>Essential Links</q-list-header>
+        <q-side-link item v-for="(link, index) in navigate" :key="index" :to="link.url">
+          <q-item-side :icon="link.icon" />
+          <q-item-main :label="link.label" :sublabel="link.sublabel" />
+        </q-side-link>
+
+      </q-list>
+    </div>
+
+    <!--
+    Replace following <div> with
+    <router-view /> component
+    if using subRoutes
+  -->
+  <router-view />
+</q-layout>
 </template>
 
 <script>
-/*
- * Root component
- */
-export default {}
+import {
+  QLayout,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QListHeader,
+  QSideLink,
+  QItem,
+  QItemSide,
+  QItemMain
+} from 'quasar'
+
+export default {
+  name: 'index',
+  components: {
+    QLayout,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QListHeader,
+    QSideLink,
+    QItem,
+    QItemSide,
+    QItemMain
+  },
+  data () {
+    return {
+      navigate: [
+        {'label': 'Pagamentos', 'sublabel': 'Últimos Pagamentos', 'url': '/payments/', 'icon': 'collections_bookmarks'},
+        {'label': 'Mensal', 'sublabel': 'Pagamentos do mês', 'url': '/Month/', 'icon': 'date_ranger'},
+        {'label': 'Parcelas', 'sublabel': 'Pagamentos Parcelados', 'url': '/Monthly/', 'icon': 'credit_card'}
+      ]
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  },
+  beforeDestroy () {
+
+  }
+}
 </script>
 
-<style></style>
+<style lang="stylus">
+
+</style>
