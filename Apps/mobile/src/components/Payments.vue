bb19ed636b780d42ef575b6d7160ae9b5f0240b7
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-12 17:42:19 -0300

    Add: Create and Remove functions

--- Apps/mobile/src/components/Payments.vue
+++ Apps/mobile/src/components/Payments.vue
@@ -1,142 +1,79 @@
 <template>
   <div>
     <h1>Pagamentos</h1>
-    <q-data-table :data="payments" :config="config" :columns="columns" @selection="selection">
-      <template slot="col-message" scope="cell">
-        <span class="light-paragraph">{{cell.data}}</span>
-      </template>
-
-      <template slot="col-source" scope="cell">
-        <div v-if="cell.data === 'Audit'" class="my-label text-white bg-primary">
-          Audit
-          <q-tooltip>Some data</q-tooltip>
-        </div>
-        <div v-else class="my-label text-white bg-negative">{{cell.data}}</div>
-      </template>
-
-      <template slot="selection" scope="props">
-        <q-btn flat color="primary" @click="changeMessage(props)">
-          <q-icon name="edit" />
-        </q-btn>
-        <q-btn flat color="primary" @click="deleteRow(props)">
-          <q-icon name="delete" />
-        </q-btn>
-      </template>
-    </q-data-table>
+    <q-fixed-position corner="top-right" :offset="[12, -28]" class="btn-add">
+      <q-btn round push color="secondary" @click="paymentCreate()" icon="add" />
+    </q-fixed-position>
+    <q-list sparse separator no-border>
+      <q-item v-for="(pay, index) in payments" :key='index' >
+        <q-item-main>
+          <q-item-tile label>{{pay.description}}</q-item-tile>
+          <q-item-tile sublabel>R$ {{pay.amount}}</q-item-tile>
+          <q-item-tile sublabel>{{pay.date}}</q-item-tile>
+        </q-item-main>
+        <q-item-side right>
+          <q-btn @click="paymentDelete(index)" round small color="red" icon="delete" />
+        </q-item-side>
+      </q-item>
+    </q-list>
   </div>
 </template>
-
 <script>
 import payments from '../datas/payments'
-import {
-  QDataTable,
-  QField,
-  QInput,
-  QCheckbox,
-  QSelect,
-  QSlider,
-  QBtn,
-  QIcon,
-  QTooltip,
-  QCollapsible
-} from 'quasar'
+import { Alert, Dialog, QList, QItem, QItemSide, QItemMain, QItemTile, QBtn, QFixedPosition } from 'quasar'
 export default {
-  components: {
-    QDataTable,
-    QField,
-    QInput,
-    QCheckbox,
-    QSelect,
-    QSlider,
-    QBtn,
-    QIcon,
-    QTooltip,
-    QCollapsible
-  },
+  components: { Alert, Dialog, QList, QItem, QItemSide, QItemMain, QItemTile, QBtn, QFixedPosition },
   data () {
     return {
       payments: payments.everything,
-      // selection: true,
-      config: {
-        columnPicker: true,
-        rowHeight: '64px',
-        responsive: true,
-        messages: {
-          noData: '<i>Não Encontrado</i> Não Há registros de Pagamentos.',
-          noDataAfterFiltering: '<i>Não Encontrado</i>Por favor redefina sua busca.'
-        },
-        selection: 'multiple',
-        // (optional) Override default labels. Useful for I18n.
-        labels: {
-          columns: 'Colunas',
-          allCols: 'Todas as Colunas',
-          rows: 'Linhas',
-          selected: {
-            singular: 'Pagamento Selecionado.',
-            plural: 'Pagamentos Selecionados.'
-          },
-          clear: 'Limpar',
-          search: 'Procurar',
-          all: 'Tudo'
-        }
-      },
-      columns: [
-        {
-          label: 'Data do Pagamento',
-          field: 'date',
-          width: '32px',
-          filter: true,
-          sort: true,
-          type: 'date',
-          format (value, row) {
-            return new Date(value).toLocaleString()
-          }
-        },
-        {
-          label: 'Descrição',
-          field: 'description',
-          width: '100px',
-          filter: true,
-          type: 'string'
-        },
-        {
-          label: 'Valor(R$)',
-          field: 'amount',
-          width: '32px',
-          filter: true,
-          type: 'number',
-          format (value, row) {
-            return 'R$ ' + value
-          }
-        }
-      ]
+      add: {'description': null, 'amount': null, 'date': null}
     }
   },
   methods: {
-    changeMessage (props) {
-      props.rows.forEach(row => {
-        row.data.message = 'Gogu'
+    paymentCreate: function () {
+      Dialog.create({
+        title: 'Adicionar Pagamento',
+        message: 'Preencha abaixo as informações para novo pagamento.',
+        form: {
+          description: {
+            type: 'text',
+            label: 'Descrição',
+            model: ''
+          },
+          amount: {
+            type: 'number',
+            label: 'Valor',
+            model: ''
+          },
+          date: {
+            type: 'text',
+            label: 'Vencimento',
+            model: ''
+          }
+        },
+        buttons: [
+          'Cancelar',
+          {
+            label: 'Salvar',
+            handler: (data) => {
+              this.payments.unshift({
+                'amount': data.amount,
+                'description': data.description,
+                'date': data.date
+              })
+              Alert.create({color: 'positive', html: 'Pagamento Criado com sucesso!'})
+            }
+          }
+        ]
       })
     },
-    deleteRow (props) {
-      props.rows.forEach(row => {
-        this.table.splice(row.index, 1)
-      })
-    },
-    refresh (done) {
-      this.timeout = setTimeout(() => {
-        done()
-      }, 5000)
-    },
-    selection (number, rows) {
-      console.log(`selected ${number}: ${rows}`)
-    },
-    rowClick (row) {
-      console.log('clicked on a row', row)
+    paymentDelete: function (index) {
+      this.payments.splice(index, 1)
+      Alert.create({color: 'info', html: 'Pagamento apagado com sucesso!'})
     }
   }
 }
 </script>
-
-<style>
+<style scoped>
+.btn-add{z-index: 2000;}
 </style>
