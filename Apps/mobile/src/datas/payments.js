532fb097d980229229049bf0ff2765374366b47b
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-13 18:24:46 -0300

    Update: Content

--- Apps/mobile/src/datas/payments.js
+++ Apps/mobile/src/datas/payments.js
@@ -20,5 +20,32 @@ export default {
       'description': 'Jantar de Aniversário de Fulano',
       'date': '2017-09-08'
     }
-  ]
+  ],
+  monthly: {
+    '2017': {
+      '09': [{'amount': 1000, 'description': 'Salário', 'date': '2017-09-10'}],
+      '08': [{'amount': 1000, 'description': 'Salário', 'date': '2017-08-10'}],
+      '07': [{'amount': 1000, 'description': 'Salário', 'date': '2017-07-10'}],
+      '06': [{'amount': 1000, 'description': 'Salário', 'date': '2017-06-10'}],
+      '05': [{'amount': 1000, 'description': 'Salário', 'date': '2017-05-10'}],
+      '04': [{'amount': 1000, 'description': 'Salário', 'date': '2017-04-10'}],
+      '03': [{'amount': 1000, 'description': 'Salário', 'date': '2017-03-10'}],
+      '02': [{'amount': 1000, 'description': 'Salário', 'date': '2017-02-10'}],
+      '01': [{'amount': 1000, 'description': 'Salário', 'date': '2017-01-10'}]
+    },
+    '2016': {
+      '12': [{'amount': 1000, 'description': 'Salário', 'date': '2016-12-10'}],
+      '11': [{'amount': 1000, 'description': 'Salário', 'date': '2016-11-10'}],
+      '10': [{'amount': 1000, 'description': 'Salário', 'date': '2016-10-10'}],
+      '09': [{'amount': 1000, 'description': 'Salário', 'date': '2016-09-10'}],
+      '08': [{'amount': 1000, 'description': 'Salário', 'date': '2016-08-10'}],
+      '07': [{'amount': 1000, 'description': 'Salário', 'date': '2016-07-10'}],
+      '06': [{'amount': 1000, 'description': 'Salário', 'date': '2016-06-10'}],
+      '05': [{'amount': 1000, 'description': 'Salário', 'date': '2016-05-10'}],
+      '04': [{'amount': 1000, 'description': 'Salário', 'date': '2016-04-10'}],
+      '03': [{'amount': 1000, 'description': 'Salário', 'date': '2016-03-10'}],
+      '02': [{'amount': 1000, 'description': 'Salário', 'date': '2016-02-10'}],
+      '01': [{'amount': 1000, 'description': 'Salário', 'date': '2016-01-10'}]
+    }
+  }
 }
