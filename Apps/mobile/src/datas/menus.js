d2e64086e4faf0ccdb3d8bd27357e3e0c325ac42
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-13 18:25:24 -0300

    Update: Item Menu

--- Apps/mobile/src/datas/menus.js
+++ Apps/mobile/src/datas/menus.js
@@ -1,6 +1,6 @@
 export default {
   navigate: [
     {'label': 'Pagamentos', 'sublabel': 'Relatórios de Pagamentos', 'url': '/payments/', 'icon': 'collections_bookmarks'},
-    {'label': 'Parcelas', 'sublabel': 'Pagamentos Futuros', 'url': '/Monthly/', 'icon': 'credit_card'}
+    {'label': 'Prestações', 'sublabel': 'Prestações Futuras', 'url': '/installments/', 'icon': 'credit_card'}
   ]
 }
