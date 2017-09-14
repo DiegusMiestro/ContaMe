6a6c9b6ca450ed5f0aa69ba04c1ee3228fcf7281
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-14 16:37:11 -0300

    Add: Periods Data

---
+++ Apps/mobile/src/datas/periods.js
@@ -0,0 +1,29 @@
+export default {
+  months: {
+    simplelist: [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro'
+    ]
+  },
+  weekdays: {
+    simplelist: [
+      'Domingo',
+      'Segunda-feira',
+      'Terça-feira',
+      'Quarta-feira',
+      'Quinta-feira',
+      'Sexta-feira',
+      'Sábado'
+    ]
+  }
+}
