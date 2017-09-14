ae49ad4c32960b60c636720d39c47df26802a2cb
Diego Feitosa <diegusmiestro@gmail.com>
2017-09-14 16:39:53 -0300

    Update: Layout and Format Date

--- Apps/mobile/src/components/Payments.vue
+++ Apps/mobile/src/components/Payments.vue
@@ -18,47 +18,75 @@
     </q-list>
     <q-modal maximized ref="addPaymentModal">
       <h4>Adicionar Pagamento</h4>
-      <q-input v-model="add.description" type="text" float-label="Descrição" />
-      <q-input v-model="add.amount" type="number" float-label="Valor (R$)" />
-      <q-datetime v-model="add.date" type="date" stack-label="Vencimento" ok-label="Confirmar" cancel-label="Cancelar" clear-label="Limpar" />
-      <q-btn color="negative" @click="$refs.addPaymentModal.close()">Cancelar</q-btn>
-      <q-btn color="positive" @click="paymentCreate(), $refs.addPaymentModal.close()">Salvar</q-btn>
+      <div class="row">
+        <q-radio class="col-4 offset-1" v-model="add.signal" val="+" label="Recebendo" color="positive" />
+        <q-radio class="col-4 offset-2" v-model="add.signal" val="-" label="Pagando" color="negative" />
+      </div>
+      <div class="row">
+        <q-input class="col-10 offset-1" v-model="add.description" type="text" float-label="Descrição do pagamento" />
+      </div>
+      <div class="row">
+        <q-input class="col-10 offset-1" v-model="add.amount" type="number" float-label="Valor do pagamento (R$)" />
+      </div>
+      <br />
+      <div class="row">
+        <q-datetime
+          class="col-10 offset-1"
+          v-model="add.date"
+          type="date"
+          stack-label="Vencimento do pagamento"
+          ok-label="Confirmar"
+          cancel-label="Cancelar"
+          clear-label=""
+          :month-names="months"
+          :day-names="weekdays"
+          format="DD/MM/YYYY"
+        />
+      </div>
+      <br />
+      <div class="row">
+        <q-btn class="col-4 offset-1" color="negative" @click="$refs.addPaymentModal.close()">Cancelar</q-btn>
+        <q-btn class="col-4 offset-2" color="positive" @click="paymentCreate(), $refs.addPaymentModal.close()">Salvar</q-btn>
+      </div>
     </q-modal>
   </div>
 </template>
 <script>
 import payments from '../datas/payments'
-import { Toast, QModal, QList, QItem, QItemSide, QItemMain, QItemTile, QBtn, QFixedPosition, QInput, QDatetime } from 'quasar'
+import periods from '../datas/periods'
+import { Toast, QModal, QList, QItem, QItemSide, QItemMain, QItemTile, QBtn, QFixedPosition, QInput, QDatetime, QRadio } from 'quasar'
 
 export default {
-  components: { Toast, QModal, QList, QItem, QItemSide, QItemMain, QItemTile, QBtn, QFixedPosition, QInput, QDatetime },
+  components: { Toast, QModal, QList, QItem, QItemSide, QItemMain, QItemTile, QBtn, QFixedPosition, QInput, QDatetime, QRadio },
   data () {
     return {
       payments: payments.everything,
-      add: {'description': null, 'amount': null, 'date': null}
+      months: periods.months.simplelist,
+      weekdays: periods.weekdays.simplelist,
+      add: {'description': null, 'amount': null, 'date': null, 'signal': null}
     }
   },
   methods: {
     paymentCreate: function () {
-      console.log(this.add)
       this.payments.unshift({
         'description': this.add.description,
-        'amount': this.add.amount,
-        'date': this.add.date
+        'amount': Number(this.add.signal + this.add.amount),
+        'date': new Date(this.add.date)
       })
       Toast.create.positive({
         html: 'Adicionado: "' + this.add.description + '"',
-        timeout: 6000
+        timeout: 3000
       })
       this.add.description = null
       this.add.amount = null
       this.add.date = null
+      this.add.signal = null
     },
     paymentDelete: function (index, pay) {
       this.payments.splice(index, 1)
       Toast.create.negative({
         html: 'Removido: "' + pay.description + '"',
-        timeout: 6000
+        timeout: 3000
       })
     }
   }
