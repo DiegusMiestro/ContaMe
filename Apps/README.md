4fb10ad9733a13e696141a5a2c884756d7dac0ed
Diego Feitosa <diegusmiestro@gmail.com>
2018-01-07 09:54:37 -0300

    Rework

--- Apps/README.md
+++
@@ -1,6 +0,0 @@
-# Diretório Apps
-
-Neste diretório ficarão os projetos da aplicação, separados por diretórios.
-
-### Mobile
-Neste diretório estará a aplicação desenvolvida para Mobile utilizando o Quasar Framework
--- Apps/mobile/src/datas/menus.js
+++
@@ -1,6 +0,0 @@
-export default {
-  navigate: [
-    {'label': 'Pagamentos', 'sublabel': 'Todos os Pagamentos', 'url': '/payments/', 'icon': 'collections_bookmarks'},
-    {'label': 'Sobre', 'sublabel': 'Informações sobre o aplicativo', 'url': '/about/', 'icon': 'help'}
-  ]
-}
